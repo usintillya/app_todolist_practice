7a9b893709b3ab93cc0bfc4bc2c8cf5128fd9ea1
Valeriy <v4ler0nnch@gmail.com>
2022-10-16 15:40:54 +0300

    -feature 'created a redirecting to the second activity_login'

--- app/src/main/java/com/example/app_todolist_practice/MainActivity.kt
+++ app/src/main/java/com/example/app_todolist_practice/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.app_todolist_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val actChange = findViewById<Button>(R.id.button_signin)
+        actChange.setOnClickListener {
+            val redirect: Intent = Intent("com.example.app_todolist_practice.LoginActivity")
+            startActivity(redirect)
+        }
     }
 }
