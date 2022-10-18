1671335ad6e63fc990d9eae3325db4bc2aa65adc
Valeriy <v4ler0nnch@gmail.com>
2022-10-18 22:46:36 +0300

    -feature 'added some changes to activities Login and Registration, created redirect to featured activity for main screen'

--- app/src/main/java/com/example/app_todolist_practice/LoginActivity.kt
+++ app/src/main/java/com/example/app_todolist_practice/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.app_todolist_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Redirecting to MainScreen activity after Login
+        val actChangeToMainScreen = findViewById<Button>(R.id.button_nextstep)
+        actChangeToMainScreen.setOnClickListener {
+            val redirect: Intent = Intent("com.example.app_todolist_practice.RegistrationActivity")
+            startActivity(redirect)
+        }
     }
 }
--- app/src/main/java/com/example/app_todolist_practice/RegistrationActivity.kt
+++ app/src/main/java/com/example/app_todolist_practice/RegistrationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.app_todolist_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        // Redirecting to LoginScreen activity after Registration
+        val actChangeToLoginScreen = findViewById<Button>(R.id.button_countinue)
+        actChangeToLoginScreen.setOnClickListener {
+            val redirect: Intent = Intent("com.example.app_todolist_practice.LoginActivity")
+            startActivity(redirect)
+        }
     }
 }
