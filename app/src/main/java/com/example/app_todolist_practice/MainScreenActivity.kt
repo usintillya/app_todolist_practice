d09edf8195ca26c58ef1fc2984179135f982ade0
Valeriy <v4ler0nnch@gmail.com>
2022-10-20 22:10:55 +0300

    -feature 'added a new popup layout and modal window'

--- app/src/main/java/com/example/app_todolist_practice/MainScreenActivity.kt
+++ app/src/main/java/com/example/app_todolist_practice/MainScreenActivity.kt
@@ -1,11 +1,27 @@
 package com.example.app_todolist_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 
 class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+        val addTodoButton = findViewById<ImageButton>(R.id.button_add)
+        addTodoButton.setOnClickListener {
+            //Inflate the dialog with custom view
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_popup_todo_add, null)
+            //Dialog Builder
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+            //Show dialog
+            val mAlertDialog = mBuilder.show()
+        }
     }
 }
