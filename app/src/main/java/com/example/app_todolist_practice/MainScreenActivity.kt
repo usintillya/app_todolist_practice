0aafaa5555940c9b4d46fa5392630e3858583f30
Valeriy <v4ler0nnch@gmail.com>
2022-10-30 06:01:53 +0300

    -fix 'fixed problem with popup corners'

--- app/src/main/java/com/example/app_todolist_practice/MainScreenActivity.kt
+++ app/src/main/java/com/example/app_todolist_practice/MainScreenActivity.kt
@@ -1,12 +1,14 @@
 package com.example.app_todolist_practice
-
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,7 @@ class MainScreenActivity : AppCompatActivity() {
             //Inflate the dialog with custom view
             val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_popup_todo_add, null)
             //Dialog Builder
-            val mBuilder = AlertDialog.Builder(this)
+            val mBuilder = AlertDialog.Builder(this, R.style.RoundedCornersDialog)
                 .setView(mDialogView)
             //Show dialog
             val mAlertDialog = mBuilder.show()
